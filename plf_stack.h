138c6963834c343908036fa03f0520ca1e4aeea4
Matt Bentley <mattreecebentley@gmail.com>
2024-07-17 14:42:38 +1200

    v2.0.7
    - Fix to Stupid copy-paste bug in operator ==, removal of needless assert in same.

--- plf_stack.h
+++ plf_stack.h
@@ -1253,11 +1253,9 @@ public:
 
 	friend bool operator == (const stack &lh, const stack &rh) PLF_NOEXCEPT
 	{
-		assert (&lh != &rh);
-
 		if (lh.total_size != rh.total_size) return false;
 
-		for (const_iterator lh_iterator = lh.begin_iterator, rh_iterator = rh.begin_iterator; lh_iterator != lh.end_iterator; ++lh_iterator, ++rh_iterator)
+		for (const_iterator lh_iterator = lh.cbegin(), rh_iterator = rh.cbegin(); lh_iterator != lh.cend(); ++lh_iterator, ++rh_iterator)
 		{
 			if (*lh_iterator != *rh_iterator) return false;
 		}
